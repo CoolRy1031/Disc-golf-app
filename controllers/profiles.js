48f659039fab50881023b1634a296de775875c77
CoolRy <that007guy@hotmail.com>
2022-06-27 15:25:30 -0400

    add user

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -15,8 +15,26 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .populate('stats')
+  .then(profile => {
+    const isSelf = profile._id.equals(req.user.profile._id)
+    res.render('profiles/show', {
+      title: `${profile.name}'s profile`,
+      profile,
+      isSelf, 
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 
 
 export{
-  index
+  index,
+  show
 }
--- models/profile.js
+++ models/profile.js
@@ -1,15 +1,20 @@
 import mongoose from 'mongoose'
 
-const profileSchema = new mongoose.Schema({
+const Schema = mongoose.Schema
+
+const profileSchema = new Schema ({
   name: String,
   avatar: String,
-  
-  
+  stats: [{ type: Schema.Types.ObjectId, ref:"Course" }],
+  strictPopulate: false,
+
+
 }, {
   timestamps: true
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
+
 
 export {
   Profile
